fac6a50ef27da2df8872f56ea534c1b836c6f7ca
rbrlpz <jrlopez233@gmail.com>
2014-10-03 14:49:44 -0500

    se agrega la navegacion hacia atras en las noticias solas

--- js/views/content-notice.js
+++ js/views/content-notice.js
@@ -2,5 +2,11 @@ WebANews.Views.ContentNotice = Backbone.View.extend({
 template : Handlebars.compile($("#fullNew").html()),
 render: function(){
 this.$el.append(this.template(this.model.toJSON()));
-}
+},
+events:{
+    "click #return":"return"
+},
+    "return" : function(){
+        WebANews.application.navigate("",{trigger:true});
+    }
 });
